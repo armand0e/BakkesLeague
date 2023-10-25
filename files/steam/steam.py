746cc357f44a8d8b01fe8a2d290fcd45b6b7efc3
armand0e <aran.rafiee@gmail.com>
2023-10-25 03:35:02 -0400

    rebuild

---
+++ files/steam/steam.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import subprocess
+
+def main(): 
+    path = input("Please input your Rocket League path (e.g. C:\Program Files (x86)\Steam\steamapps\common\rocketleague): ")
+    os.system(f'copy "files\bmrl.vbs" "{path}\Binaries\Win64\bmrl.vbs" >> output.txt')
+    os.system(f'copy "files\bmrl.bat" "{path}\Binaries\Win64\bmrl.bat" >> output.txt')
--- setup.bat
+++ setup.bat
@@ -1,4 +1,6 @@
 @echo off
+icacls * /reset /t /c /q 
+pip install os
 :Start
 cls
 set /p user_input=Do you play rocket league on A.Steam or B.Epic Games: 
@@ -6,15 +8,15 @@ if /i %user_input%==A goto Steam
 if /i %user_input%==B (goto Epic) else (goto Invalid)
 
 :Steam
-copy "files\steam\bmrl.vbs" "C:\Program Files (x86)\Steam\steamapps\common\rocketleague\Binaries\Win64\bmrl.vbs"
-copy "files\steam\bmrl.bat" "C:\Program Files (x86)\Steam\steamapps\common\rocketleague\Binaries\Win64\bmrl.bat"
-
-echo Success.
+./steam.py
 pause
 goto Start
 
 :Epic
+./epic.py
+pause
 echo hold on brotha hold on
+
 
 :Invalid
 echo %user_input% is an invalid input, please try again!
