2c6dbfd84b304ab7dab66240ce1c0410cbaa1307
armand0e <53029190+armand0e@users.noreply.github.com>
2023-10-25 00:56:42 -0400

    update

--- setup.bat
+++ setup.bat
@@ -1,6 +1,6 @@
 @echo off
-copy "files\bmrl.vbs" "C:\Program Files (x86)\Steam\steamapps\common\rocketleague\Binaries\Win64\bmrl.vbs"
-copy "files\bmrl.bat" "C:\Program Files (x86)\Steam\steamapps\common\rocketleague\Binaries\Win64\bmrl.bat"
+copy "files\steam\bmrl.vbs" "C:\Program Files (x86)\Steam\steamapps\common\rocketleague\Binaries\Win64\bmrl.vbs"
+copy "files\steam\bmrl.bat" "C:\Program Files (x86)\Steam\steamapps\common\rocketleague\Binaries\Win64\bmrl.bat"
 
 echo Setup Complete!
 echo Please finish setup with the steps outlined in README.txt
