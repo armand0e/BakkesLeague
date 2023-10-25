d4fe04019e21656310628b28f2c56e93eb3f0817
Arman Rafiee <aran.rafiee@gmail.com>
2023-10-25 00:37:14 -0400

    Add files via upload

---
+++ setup.bat
@@ -0,0 +1,7 @@
+@echo off
+copy "files\bmrl.vbs" "C:\Program Files (x86)\Steam\steamapps\common\rocketleague\Binaries\Win64\bmrl.vbs"
+copy "files\bmrl.bat" "C:\Program Files (x86)\Steam\steamapps\common\rocketleague\Binaries\Win64\bmrl.bat"
+
+echo Setup Complete!
+echo Please finish setup with the steps outlined in README.txt
+pause
