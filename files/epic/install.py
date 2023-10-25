6895e1714736c8853acc17dcc3de8f8233a7c6af
armand0e <53029190+armand0e@users.noreply.github.com>
2023-10-25 18:43:05 -0400

    better than ever

--- files/epic/install.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-import subprocess
-
-path = input("Please input your Rocket League path (default: C:\Program Files\Epic Games\rocketleague): ")
-os.system(f'copy "files\bmrl.vbs" "{path}\Binaries\Win64\bmrl.vbs"')
-os.system(f'copy "files\steam\bmrl.bat" "{path}\Binaries\Win64\bmrl.bat"')
--- setup.bat
+++ setup.bat
@@ -1,21 +1,54 @@
 @echo off
 icacls * /reset /t /c /q 
+cls
 
 :Start
-cls
-set /p user_input=Do you play rocket league on A.Steam or B.Epic Games: 
-if /i %user_input%==A goto Steam
-if /i %user_input%==B (goto Epic) else (goto Invalid)
+:: Entrypoint; ask user for platform
+set /p user_input=Do you play rocket league on A.Steam or B.Epic Games (Enter A or B): 
+if %user_input%==A goto Steam
+if %user_input%==B (goto Epic) else (goto Invalid)
 
 :Steam
-python files\steam\install.py
+:: Steam setup starts
+setlocal
+echo. Please select the location of your Rocket League folder. &>nul timeout /t 1
+
+set "psCommand="(new-object -COM 'Shell.Application')^
+.BrowseForFolder(0,'Please select the location of your Rocket League folder.',0,0).self.path""
+
+for /f "usebackq delims=" %%I in (`powershell %psCommand%`) do set "RLfolder=%%I"
+
+setlocal enabledelayedexpansion
+echo. You selected: "%RLfolder%"
+:: rocketleague folder is selected and stored into %RLfolder%
+copy "files\bmrl.vbs" "%RLfolder%\Binaries\Win64\bmrl.vbs"
+copy "files\steam\bmrl.bat" "%RLfolder%\Binaries\Win64\bmrl.bat"
+:: Files are copied to the rocketleague\Binaries\Win64 folder
 pause
+:: Steam launch options script
+
 goto Start
 
 :Epic
-python files\epic\install.py
+:: Epic Games setup starts
+setlocal
+echo Please select the location of your Rocket League folder. &>nul timeout /t 1
+
+set "psCommand="(new-object -COM 'Shell.Application')^
+.BrowseForFolder(0,'Please select the location of your Rocket League folder.',0,0).self.path""
+
+for /f "usebackq delims=" %%I in (`powershell %psCommand%`) do set "RLfolder=%%I"
+
+setlocal enabledelayedexpansion
+echo You selected: "%RLfolder%"
+:: rocketleague folder is selected and stored into %RLfolder%
+copy "files\bmrl.vbs" "%RLfolder%\Binaries\Win64\bmrl.vbs"
+copy "files\epic\bmrl.bat" "%RLfolder%\Binaries\Win64\bmrl.bat"
+:: Files are copied to the rocketleague\Binaries\Win64 folder
 pause
-echo hold on brotha hold on
+:: Epic Games launch options script
+
+goto Start
 
 :Invalid
 echo %user_input% is an invalid input, please try again!
