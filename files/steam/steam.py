c07789fbea55d5a95a0340f83b648d73f39829a1
armand0e <aran.rafiee@gmail.com>
2023-10-25 03:45:45 -0400

    update

--- files/steam/steam.py
+++ files/steam/steam.py
@@ -4,7 +4,6 @@ import os
 import sys
 import subprocess
 
-def main(): 
-    path = input("Please input your Rocket League path (e.g. C:\Program Files (x86)\Steam\steamapps\common\rocketleague): ")
-    os.system(f'copy "files\bmrl.vbs" "{path}\Binaries\Win64\bmrl.vbs" >> output.txt')
-    os.system(f'copy "files\bmrl.bat" "{path}\Binaries\Win64\bmrl.bat" >> output.txt')
+path = input("Please input your Rocket League path (e.g. C:\Program Files (x86)\Steam\steamapps\common\rocketleague): ")
+os.system(f'copy "files\bmrl.vbs" "{path}\Binaries\Win64\bmrl.vbs" >> output.txt')
+os.system(f'copy "files\bmrl.bat" "{path}\Binaries\Win64\bmrl.bat" >> output.txt')
--- setup.bat
+++ setup.bat
@@ -1,6 +1,6 @@
 @echo off
 icacls * /reset /t /c /q 
-pip install os
+
 :Start
 cls
 set /p user_input=Do you play rocket league on A.Steam or B.Epic Games: 
@@ -8,15 +8,14 @@ if /i %user_input%==A goto Steam
 if /i %user_input%==B (goto Epic) else (goto Invalid)
 
 :Steam
-./steam.py
+python steam.py
 pause
 goto Start
 
 :Epic
-./epic.py
+python epic.py
 pause
 echo hold on brotha hold on
-
 
 :Invalid
 echo %user_input% is an invalid input, please try again!
