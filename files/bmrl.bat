8b36a0386d01d87cde7cb1cc54b50aadb87f208a
armand0e <53029190+armand0e@users.noreply.github.com>
2023-10-25 21:57:40 -0400

    Update bmrl.bat

--- files/bmrl.bat
+++ files/bmrl.bat
@@ -2,11 +2,11 @@
 start "" "E:\SteamLibrary\steamapps\common\rocketleague\Binaries\Win64\rocketleague.exe" 
 start "" "C:\Program Files\BakkesMod\BakkesMod.exe" 
 :check_process_running
-timeout /t 10 /nobreak >nul 2>&1 
+timeout /t 30 /nobreak >nul 2>&1 
 tasklist /NH /FI "IMAGENAME eq rocketleague.exe" 2>nul | find /I /N "rocketleague.exe">nul 
 if not "%ERRORLEVEL%"=="1" goto check_process_running 
 
 timeout /t 10 /nobreak >nul 2>&1
 taskkill /im "BakkesMod.exe" /f
 
-pause 
+pause
