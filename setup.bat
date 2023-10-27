25337e741b3f440d71bed96de6a2b3f254544657
armand0e <53029190+armand0e@users.noreply.github.com>
2023-10-27 05:30:02 -0400

    update
    looks better.
    
    check for bakkesmod.exe doesnt seem to have proper syntax

--- setup.bat
+++ setup.bat
@@ -53,10 +53,7 @@ type files\check.txt >> files\bakkesleague.bat
 
 :: default bakkesleague.bat
  is created
-copy "files\bakkesleague.bat
-" "C:\Program Files (x86)\Steam\steamapps\common\rocketleague\Binaries\Win64\bakkesleague.bat
-" 
-copy "files\check.bat" "C:\Program Files (x86)\Steam\steamapps\common\rocketleague\Binaries\Win64\check.bat"
+copy "files\bakkesleague.bat" "C:\Program Files (x86)\Steam\steamapps\common\rocketleague\Binaries\Win64\bakkesleague.bat" 
 cls
 echo install complete!
 :: Steam launch options default setup script
@@ -106,10 +103,7 @@ type files\check.txt >> files\bakkesleague.bat
 :: custom bakkesleague.bat
  is created
 copy "files\bakkesleague.vbs" "%RLfolder%\Binaries\Win64\bakkesleague.vbs"
-copy "files\check.bat" "%RLfolder%\Binaries\Win64\check.bat"
-copy "files\bakkesleague.bat
-" "%RLfolder%\Binaries\Win64\bakkesleague.bat
-"
+copy "files\bakkesleague.bat" "%RLfolder%\Binaries\Win64\bakkesleague.bat"
 
 cls
 echo install complete!
@@ -132,15 +126,14 @@ if /i %user_input%==q (goto Exit) else (goto EpicContinue)
 
 :EpicContinue
 :: Epic Games setup starts
-copy "files\bakkesleague.vbs" "C:\Program Files\Epic Games\rocketleague\Binaries\Win64\bakkesleague.vbs"
+
 cls
 :: Check for success
-if exist "C:\Program Files\Epic Games\rocketleague\Binaries\Win64\bakkesleague.vbs" (goto EpicDefault) else (goto Custom)
+if exist "C:\Program Files\Epic Games\rocketleague\Binaries\Win64\" (goto EpicDefault) else (goto Custom)
 
 
 :EpicDefault
 :: create bakkesleague.bat with default paths
- is created
 echo @echo off > files\bakkesleague.bat
 
 echo start "" "C:\Program Files\Epic Games\rocketleague\Binaries\Win64\rocketleague.exe" >> files\bakkesleague.bat
@@ -168,13 +161,9 @@ echo start "" "%bakkesfolder%\BakkesMod.exe" >> files\bakkesleague.bat
 type files\check.txt >> files\bakkesleague.bat
 
 
-:: bakkesleague.bat
- is copied to rocketleague folder 
-copy "files\bakkesleague.bat
-" "C:\Program Files\Epic Games\rocketleague\Binaries\Win64\bakkesleague.bat
-" 
-copy "files\check.bat" "C:\Program Files\Epic Games\rocketleague\Binaries\Win64\check.bat"
-
+:: bakkesleague.bat is copied to rocketleague folder 
+copy "files\bakkesleague.bat" "C:\Program Files\Epic Games\rocketleague\Binaries\Win64\bakkesleague.bat" 
+copy "files\bakkesleague.vbs" "C:\Program Files\Epic Games\rocketleague\Binaries\Win64\bakkesleague.vbs"
 cls
 echo install complete!
 :: Epic Games launch options default setup script
