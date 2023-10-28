32e64f44a0d775c6b63a40fa300b1132b54524e6
armand0e <aran.rafiee@gmail.com>
2023-10-28 15:21:54 -0400

    Update new.bat

--- new.bat
+++ new.bat
@@ -4,19 +4,19 @@ cls
 :: platform codes are A=steam, B=epic, Z=unknown
 set platform=Z
 :: check for default paths
-:steamcheck
-if exist "C:\Program Files (x86)\Steam\steamapps\common\rocketleague" set platform=A && set "RLfolder=C:\Program Files (x86)\Steam\steamapps\common\rocketleague" 
+if exist "C:\Program Files (x86)\Steam\steamapps\common\rocketleague" (set platform=A && set "RLfolder=C:\Program Files (x86)\Steam\steamapps\common\rocketleague")
 if exist "C:\Program Files\Epic Games\rocketleague" (
     if %platform%==A (
         echo You have both steam and epic games rocket league installed. Please select which one you would like to use.
         set /p user_input=Do you want to install bakkesleague on A.Steam or B.Epic Games (Enter A or B): 
-        if /i %user_input%==A (set platform=1) else (set platform=2)
+        if /i %user_input%==A (set platform=A) else (set platform=B)
     )
     if %platform%==B (
         set "RLfolder=C:\Program Files\Epic Games\rocketleague"
         echo this worked *epic*
-        pause
+        
     )
+    
  ) 
 :b
 if %platform%==Z (
