ce01cea7c3e617c50269b61892be425005dd70a9
armand0e <aran.rafiee@gmail.com>
2023-10-28 18:20:47 -0400

    update
    moved setup_old into history
    
    also added a batch for bat to exe conversion in history
    
    I wasnt able to use it on my mac but try making an exe out of setup.bat

--- setup.bat
+++ setup.bat
@@ -31,6 +31,9 @@ pause
 set /p user_input=press enter to rerun setup.bat, or press q to quit:
 if /i %user_input%==q (goto quit) else (goto Start)
 
+:: End of script
+
+:: EVERYTHING BELOW ARE FUNCTIONS ::
 
 :Invalid
 echo Error: %user_input% is an invalid, please try again!
@@ -45,23 +48,30 @@ if exist "C:\Program Files (x86)\Steam\steamapps\common\rocketleague\Binaries\Wi
 :: either the default steam directory is chosen and user is sent to :bakkescheck
 :: or the user is routed to :Custom
 
+
+
 :Epic
 set platform=Epic
 if exist "C:\Program Files\Epic Games\rocketleague\Binaries\Win64\rocketleague.exe" (set "RLfolder=C:\Program Files\Epic Games\rocketleague" && goto bakkescheck) else (goto Custom)
 :: either the default epic directory is chosen and user is sent to :bakkescheck
 :: or the user is routed to :Custom
 
+
+
 :Custom
 setlocal
-echo Error: rocketleague folder not found in the default %platform% directory.
+echo Error: rocketleague.exe not found.
 echo Please select the location of your "rocketleague" folder. &>nul timeout /t 1
 set "psCommand="(new-object -COM 'Shell.Application')^
 .BrowseForFolder(0,'Please select the location of your rocketleague folder.',0,0).self.path""
 for /f "usebackq delims=" %%I in (`powershell %psCommand%`) do set "RLfolder=%%I"
 setlocal enabledelayedexpansion
 :: rocketleague folder is selected and stored into %RLfolder%
-goto bakkescheck
-:: user is routed to :bakkescheck
+if exist "%RLfolder%\Binaries\Win64\rocketleague.exe" (goto bakkescheck) else (goto Custom)
+:: checks if rocketleague.exe is in %RLfolder%
+:: if it is, then user is routed to :bakkescheck
+:: if it is not, then user is routed to :Custom
+
 
 
 :bakkescheck
@@ -69,17 +79,21 @@ if exist "%RLfolder%\Binaries\Win64\bakkesmod.exe" (set "bakkesfolder=%RLfolder%
 :: if bakkesmod.exe is found in %RLfolder%\Binaries\Win64, then %bakkesfolder% is created and user is sent to :bakkescheckreturn
 :: if bakkesmod.exe is not found in %RLfolder%\Binaries\Win64, then user is sent to :bakkescustom
 
+
+
 :bakkescustom
 setlocal
-echo Error: BakkesMod folder not found in the default directory.
+echo Error: BakkesMod.exe not found.
 echo Please select the location of your BakkesMod folder. (the one that has "BakkesMod.exe") &>nul timeout /t 1
 set "psCommand="(new-object -COM 'Shell.Application')^
 .BrowseForFolder(0,'Please select the location of your "BakkesMod" folder.',0,0).self.path""
 for /f "usebackq delims=" %%I in (`powershell %psCommand%`) do set "bakkesfolder=%%I"
 setlocal enabledelayedexpansion
 :: BakkesMod folder is selected and stored into %bakkesfolder%
-goto bakkescheckreturn
-
+if exist "%bakkesfolder%\BakkesMod.exe" (goto bakkescheckreturn) else (goto bakkescustom)
+:: checks if BakkesMod.exe is in %bakkesfolder%
+:: if it is, then user is routed to :bakkescheckreturn
+:: if it is not, then user is routed to :bakkescustom
 
 
 :quit
